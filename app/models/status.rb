b466601616869e0f5696b8b4121c49a5aeba7e82
Wyatt Melin <wyattmelin@yahoo.com>
2014-03-25 16:26:03 -0600

    Created a rails application and added status updates

---
+++ app/models/status.rb
@@ -0,0 +1,4 @@
+class Status < ActiveRecord::Base
+	attr_accessible :content, :name
+	
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Netbook::Application.config.session_store :cookie_store, key: '_netbook_session'
